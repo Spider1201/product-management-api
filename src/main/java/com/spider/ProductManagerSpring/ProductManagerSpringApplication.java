e79cd887f2caa434da30ad30115f596225a978a5
Onatade Adeyincka Abdulmajeed <onatadeabdulmajeed@gmail.com>
2026-06-30 16:38:58 +0100

    Add Spring Data JPA and PostgreSQL dependencies

---
+++ src/main/java/com/spider/ProductManagerSpring/ProductManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spider.ProductManagerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spider/ProductManagerSpring/ProductManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spider.ProductManagerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
